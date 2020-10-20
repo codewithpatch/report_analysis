29ce1e1d0226652ea46941fed9d0a1f9c501f402
codewithpatch <chesterdealday@gmail.com>
2020-10-20 16:21:33 +0200

    Implementation of RUN_ALL and Running specific reports for debugging purposes.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import logging
+
 from pipelines.exception_records import ExceptionRecordsPipeline
 from pipelines.matched import MatchedPipeline
 from pipelines.mismatch import MismatchPipeline
 from pipelines.mx import MissingInMXPipeline
 from pipelines.saa import MissingInSaaPipeline
 from pipelines.unclassified_exception import UnclassifiedExceptionPipeline
+from settings import REPORTS, RUN_ALL
 
 if __name__ == '__main__':
     pipes = [
@@ -17,8 +20,11 @@ if __name__ == '__main__':
     # reader = ReportReader('ExceptionRecords')
 
     for PipeClass in pipes:
-        if PipeClass.report != 'MissingInMX':
+        if not RUN_ALL and not REPORTS[PipeClass.report]:
+            logging.info(f"Skipping Report analysis for {PipeClass.report}...")
             continue
+
+        logging.info(f"Running Report analysis for {PipeClass.report}...")
 
         pipeline = PipeClass()
         pipeline.process_df()
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from datetime import date
+from datetime import date, timedelta
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-6s %(message)s',
@@ -13,6 +13,36 @@ logging.basicConfig(
 # DATE = date.today()
 DATE = date(2020, 10, 19)
 
+
+def prev_weekday(adate: date) -> date:
+    adate -= timedelta(days=1)
+    while adate.weekday() > 4:  # Mon-Fri are 0-4
+        adate -= timedelta(days=1)
+
+    # Format date 2020-10-16 or date(2020, 10, 16)
+    return adate
+
+
+DATE_TO_PROCESS = prev_weekday(DATE)
+
+# TOGGLE WHICH REPORTS TO RUN
+'''
+RUN_ALL
+    1. If True, It will run all reports in the REPORTS dict regardless of the toggle
+    2. If False, Check which reports to run in REPORTS dict:
+        1. If toggle is 1 -> Run report analysis
+        2. If toggle is 0 -> Skip running report analysis
+'''
+RUN_ALL = False
+REPORTS = {
+    'ExceptionRecords': 1,
+    'Matched': 0,
+    'MISMATCH': 0,
+    'MissingInMX': 0,
+    'MissingInSAA': 0,
+    'UnclassifiedException': 0
+}
+
 ROOT_DIR = os.path.dirname(__file__)
 SRC_DIR = os.path.join(ROOT_DIR, 'src')
 
@@ -23,7 +53,6 @@ FXMM_DIR = os.path.join(SRC_DIR, 'FXMM')
 MASTER_DIR = os.path.join(SRC_DIR, 'MASTER')
 
 TEAMS = ['BONDS', 'EQD STRUCT', 'FXMM']
-REPORTS = ['ExceptionRecords', 'MISMATCH', 'UnclassifiedException']
 
 # OUTPUT DIRECTORY
 OUT_DIR = os.path.join(ROOT_DIR, 'output')
